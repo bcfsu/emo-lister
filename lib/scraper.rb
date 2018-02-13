1fc2fb05e869ac1874ef14e4f4b4935eedcf3645
britt <britt.cumens@gmail.com>
2018-02-13 16:09:00 -0500

    fix URL assignment to episodes. capybara/poltergeist scraping fix. assign summaries to episode instances

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,29 +5,31 @@ class EmoScraper
 
     episodes = site.css("h1.entry-title a")
     episodes.collect do |episode|
-      index = "www.washedupemo.com"
+      index = "https://www.washedupemo.com"
       name = episode.text
       url = index + episode.attr("href").strip
       new_epi = Episode.new(name, url)
-      new_epi.summary = "This is a summary of #{new_epi.name}"
+      # new_epi.summary = "This is a summary of #{new_epi.name}"
       new_epi
     end
 
     # TODO: Next Steps: Scrape the summary page and assign each Episode a :summary
 
-    # Episode.all.collect do |episode|
+    Episode.all.collect do |episode|
 
-    #   # episode_page = Nokogiri::HTML(open("#{episode.url}"))
+      # episode_page = Nokogiri::HTML(open("#{episode.url}"))
 
-    #   # FIXME: Capybara::Poltergeist::StatusFailError ... The visit command is not functioning,
-    #   # I am getting a DNS timeout
-    #
-    #   session = Capybara::Session.new(:poltergeist)
-    #   session.visit("http://www.washedupemo.com/news/2018/5/ep-123-buddy-nielsen-senses-fail")
-    #   element = session.first("p.description")
-    #   episode.summary = element.text.strip
-    #   episode
-    # end
+      # FIXME: Capybara::Poltergeist::StatusFailError ... The visit command is not functioning,
+      # I am getting a DNS timeout
+
+      session = Capybara::Session.new(:poltergeist)
+      # binding.pry
+      session.visit("#{episode.url}")
+      # session.has_content?('p.description')
+      element = session.first("p.description")
+      episode.summary = element.text.strip
+      episode
+    end
 
     # This method should be able to return Episode.all
     # Episode.all[0].summary = "This episode is..."
